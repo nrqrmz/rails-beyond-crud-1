2a7e506e0fa6ac3b88220968993d64834c769a98
Enrique Ramirez <nrqrmz@gmail.com>
2022-08-18 00:30:56 -0500

    update create action to display errors

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,9 +7,14 @@ class ReviewsController < ApplicationController
 
   def create
     @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review.restaurant = @restaurant
-    @review.save
-    redirect_to restaurant_path(@restaurant)
+    
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
